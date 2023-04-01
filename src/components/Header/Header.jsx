4f3db713303949d3191e7d34f7b923a607c35a26
Zuzanna Kaźmierczak <zusapphire@gmail.com>
2023-04-01 14:48:29 +0200

    Add header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,22 @@
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 import css from './Header.module.css';
 
 export const Header = () => {
-    return (
-      <header>
-        <nav>
-          <NavLink to='/'>Homepage</NavLink>
-          <NavLink to='/movies'>Movies</NavLink>
-        </nav>
-      </header>
-    );
-}
+  return (
+    <header>
+      <nav className={css.nav_1}>
+        <NavLink to="/">Homepage</NavLink>
+        <div className={css.border}></div>
+        <NavLink to="/movies">Movies</NavLink>
+      </nav>
+      <nav className={css.nav_2}>
+        <button type="button">
+          <NavLink>Go back</NavLink>
+        </button>
+        <button type="button">
+          <NavLink>Go forward</NavLink>
+        </button>
+      </nav>
+    </header>
+  );
+};
