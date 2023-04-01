4f299315f21ec31981fc8e41b6424b5a8e767249
Zuzanna Kaźmierczak <zusapphire@gmail.com>
2023-04-01 14:48:35 +0200

    Add home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import fetchData from 'utils/fetchData';
 import { Spinner } from 'components/Spinner/Spinner';
+import './Home.module.css';
 
 export const Home = () => {
   const URL = `https://api.themoviedb.org/3/trending/movie/day?api_key=16b877ef666ac997a00ad8a16d1abc88`;
