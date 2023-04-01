31099c3702dd36f2bcac169875b70cec6dd8f766
Zuzanna Kaźmierczak <zusapphire@gmail.com>
2023-04-01 14:48:08 +0200

    Change Routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, NavLink } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Cast } from '../Cast/Cast';
 import { Home } from '../Home/Home';
 import { MovieDetails } from '../MovieDetails/MovieDetails';
@@ -6,15 +6,16 @@ import { Movies } from '../Movies/Movies';
 import { Reviews } from '../Reviews/Reviews';
 import { NotFound } from '../NotFound/NotFound';
 import { Header } from 'components/Header/Header';
+import 'index.css';
 
 export const App = () => {
   return (
-    <div>
+    <div className="wrapper">
       <Header />
       <Routes>
-        <Route index element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
-        <Route path="movies/:movieId" element={<MovieDetails />}>
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
