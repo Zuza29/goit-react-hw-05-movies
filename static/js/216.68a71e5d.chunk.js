5f9a559b0f14b612017df7059d4275c16bc80676
Zuza29 <zusapphire@gmail.com>
2023-04-02 12:47:15 +0000

    Deploying to gh-pages from @ Zuza29/goit-react-hw-05-movies@1ce3dbfee02c74dc710fa8afc165f6732edcdc2b 🚀

--- static/js/216.68a71e5d.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[216],{216:function(e,t,r){r.r(t);var n=r(184);t.default=function(){return(0,n.jsxs)(n.Fragment,{children:[(0,n.jsx)("b",{children:"404"}),(0,n.jsx)("p",{children:"Sorry, we couldn't find that page :("})]})}}}]);
-//# sourceMappingURL=216.68a71e5d.chunk.js.map
